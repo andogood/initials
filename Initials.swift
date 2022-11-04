0d7675776f4a13aab2173c47e682a9e003cfb178
Andrew Goodman <newaccount.puf8k@simplelogin.co>
2022-11-04 20:51:52 +1100

    Add files via upload

---
+++ Initials.swift
@@ -0,0 +1,19 @@
+Initials.swift
+
+// I am awesome - I am passionate about ligma-itis
+
+print("  A  ")
+print(" A A ")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
+
+print(" GGG ")
+print("G   G")
+print("G    ")
+print("G GGG")
+print("G   G")
+print("G   G")
+print(" GGG ")
